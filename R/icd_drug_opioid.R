c5f4da9abba080ca64323369d638e7d3b54c6d0c
Mamadou Ndiaye <epinotes@gmail.com>
2018-12-26 12:06:32 -0800

    Add files via upload
    simplify icd_drug_opioid

--- R/icd_drug_opioid.R
+++ R/icd_drug_opioid.R
@@ -14,13 +14,13 @@
 #' filter(hosp_set, year == 2016, quarters(discharge_date) == "Q4" ) %>% od_drug_apr_icd10cm(diag_ecode_col = c(3, 6)) %>% sample_n(5)
 #'
 icd_drug_opioid <- function(data, diag_ecode_col) {
-  drugs_icd10cm_ <- "^(?!(T3[679]9|T414|T427|T4[3579]9))(T3[6-9]|T4[0-9]|T50)..(1|2|3|4)(A|$)|((T3[679]9|T414|T427|T4[3579]9)(1|2|3|4).(A|$))"
+  drugs_icd10cm_ <- "^(?!(T3[679]9|T414|T427|T4[3579]9))(T3[6-9]|T4[0-9]|T50)..[1-4](A|$)|((T3[679]9|T414|T427|T4[3579]9)[1-4].(A|$))"
 
-  opioid_icd10cm_ <- "(T40[01234].|T406[09])(1|2|3|4)(A|$)"
+  opioid_icd10cm_ <- "(T40[01234].|T406[09])[1-4](A|$)"
 
-  non_heroin_opioid_icd10cm_ <- "(T40[0234].|T406[09])(1|2|3|4)(A|$)"
+  non_heroin_opioid_icd10cm_ <- "(T40[0234].|T406[09])[1-4](A|$)"
 
-  heroin_icd10cm_ <- "T401.(1|2|3|4)(A|$)"
+  heroin_icd10cm_ <- "T401.[1-4](A|$)"
 
 
   data %>%
